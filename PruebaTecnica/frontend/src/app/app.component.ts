0136c721c041518a990d408b18253a4b41268a26
JuanVerdooren <jlverdooren12@gmail.com>
2024-12-01 19:52:47 +0000

    Guardando cambios antes de actualizar Angular

---
+++ PruebaTecnica/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',  // Enlazamos la plantilla HTML
+  styleUrls: ['./app.component.css']    // Enlazamos el archivo CSS
+})
+export class AppComponent {
+  title = 'PruebaTecnica';  // Título de la aplicación, puedes modificarlo si lo deseas
+}
