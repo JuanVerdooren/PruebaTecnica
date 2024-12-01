89def32f3cd995e5893f5a2937e13da24fe1ce4d
JuanVerdooren <jlverdooren12@gmail.com>
2024-12-01 18:51:32 +0000

    Guardando cambios antes de actualizar Angular

--- PruebaTecnica/frontend/src/app/app.module.ts
+++ PruebaTecnica/frontend/src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { PantallaBusquedaComponent } from './components/pantalla-busqueda/pantalla-busqueda.component';
 import { PantallaResumenComponent } from './components/pantalla-resumen/pantalla-resumen.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/busqueda', pathMatch: 'full' },
+  { path: 'busqueda', component: PantallaBusquedaComponent },
+  { path: 'resumen', component: PantallaResumenComponent }
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    PantallaBusquedaComponent,
-    PantallaResumenComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule { }
